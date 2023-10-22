458745a95d17daf9060b7285ef4d80b71e63f9de
Ali Abodaraa <abodaraaali50@gmail.com>
2023-10-22 12:09:08 +0300

    add bootstrap template and navbase component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFireModule } from '@angular/fire/compat';
-import { enviroment } from 'src/enviroments/enviroment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+import { enviroment } from 'src/enviroments/enviroment.prod';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BsNavbarComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(enviroment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,3 +1,12 @@
 export const enviroment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyCDlI9IlyVRCVRT-LhebENUNWUrvH2UfRQ',
+    authDomain: 'o-sho-5b1ba.firebaseapp.com',
+    projectId: 'o-sho-5b1ba',
+    storageBucket: 'o-sho-5b1ba.appspot.com',
+    messagingSenderId: '574548078746',
+    appId: '1:574548078746:web:1a1a13ebec6ad61d422f23',
+    measurementId: 'G-4992WB1D5J',
+  },
 };
